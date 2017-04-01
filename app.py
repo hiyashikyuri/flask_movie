a3696fa5bcbe16790a82f69f8d472ce8f721cfed
大久保 将広 <Masahiro@okubo.local>
2017-04-01 19:39:54 +0900

    added code for sql

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flask-movie'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/')
+def index():
+    return "<h1 style='color: red'>hello flask</h1>"
+
+
+@app.route('/')
+def index():
+    return "<h1 style='color: red'>hello flask</h1>"
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
